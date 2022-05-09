84762430069c2b86c57d115d0b96fa5e4971c8a6
svenne1482 <erik.perers@outlook.com>
2022-05-10 00:03:21 +0200

    Added multiple questions

--- js/game.js
+++ js/game.js
@@ -1,3 +1,6 @@
+var currentQuestionIndex;
+var questionsList;
+
 const rippleTitle = async () =>
 {
     var title = document.getElementById("title");
@@ -25,9 +28,11 @@ const apiRequest = async (url) =>
 const fetchTrivia = async () => 
 {
     var json = await apiRequest('https://opentdb.com/api.php?amount=10&category=21&difficulty=medium&type=multiple');
-    var questions = json.results;
-    var image = await fetchImage(questions[0].category);
-    createTriviaCard(questions[0].question,questions[0].correct_answer,questions[0].incorrect_answers,image);
+        questionsList = json.results;
+    currentQuestionIndex = 0;
+    var currentQuestion = questionsList[currentQuestionIndex];
+    var image = await fetchImage(currentQuestion.category);
+    createTriviaCard(currentQuestion.question,currentQuestion.correct_answer,currentQuestion.incorrect_answers,image);
 
 }
 
@@ -51,6 +56,7 @@ const createTriviaCard = async (q,correct,ansList,imageUrl) =>
         var text = document.createElement("p");
         text.innerHTML = ansList[i];
         answer.appendChild(text);
+        answer.onclick = submitAnswer;
         answers.appendChild(answer);
     }
     var image = document.createElement("img");
@@ -72,8 +78,55 @@ function shuffleArray(array) {
 
 const fetchImage = async (category) =>
 {
-    var json = await apiRequest('https://api.unsplash.com/photos/random?client_id=9CBJBoPA6QIPQXRIjYELTmDXNZvYybMj-XxUmdeVPNI&query='+category);
-    return json.urls.small;
+    return "./content/dota2.jpg"
+    //var json = await apiRequest('https://api.unsplash.com/photos/random?client_id=9CBJBoPA6QIPQXRIjYELTmDXNZvYybMj-XxUmdeVPNI&query='+category);
+    //return json.urls.small;
+}
+
+const submitAnswer = async (e) =>
+{
+    var element = e.srcElement
+    while(element.children.length > 0)
+        element = element.children[0];
+
+    validateAnswer(element.innerHTML);
+    createNextButton();
+}
+
+const validateAnswer = async (ans) =>
+{
+    if(ans == questionsList[currentQuestionIndex].correct_answer)
+        console.log("hi");
+}
+
+const createNextButton = async () => {
+    var button = document.getElementById("next-button")
+    if(button)
+        return;
+    var board = document.getElementById("single-player-board");
+    var nxtButton = document.createElement("button");
+    nxtButton.innerHTML = "Next Question";
+    nxtButton.id = "next-button";
+    nxtButton.onclick = nextTrivia;
+    board.appendChild(nxtButton);
+}
+
+const nextTrivia = async (e) => {
+    removeCurrentQuestion(e.srcElement);
+    if(currentQuestionIndex < 9)
+    {
+        console.log("hi");
+        currentQuestionIndex = currentQuestionIndex+1;
+        var currentQuestion = questionsList[currentQuestionIndex];
+        var image = await fetchImage(currentQuestion.category);
+        createTriviaCard(currentQuestion.question,currentQuestion.correct_answer,currentQuestion.incorrect_answers,image);
+    }
+}
+
+const removeCurrentQuestion = async (sender) => {
+    var old = document.getElementsByClassName("trivia-card")[0];
+    old.remove();
+    sender.remove();
 }
 
 const delay = ms => new Promise(res => setTimeout(res, ms));
